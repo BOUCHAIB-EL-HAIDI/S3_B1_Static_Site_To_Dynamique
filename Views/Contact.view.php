03adb4d8c63c89e1414c686bb3c50a5865c81a09
BOUCHAIB EL HAIDI <bouchaibelhaidi1@gmail.com>
2025-12-04 12:00:57 +0100

    getting the info from the form and save it into contacts.json

--- Views/Contact.view.php
+++ Views/Contact.view.php
@@ -3,11 +3,11 @@
 
   <section class="container mx-auto py-16">
     <h2 class="text-3xl font-bold mb-6 text-center">Contactez-nous</h2>
-    <form class="max-w-xl mx-auto bg-white p-8 shadow-md rounded-lg space-y-4">
-      <input type="text" placeholder="Votre nom" class="w-full border px-4 py-2 rounded-lg">
-      <input type="email" placeholder="Votre email" class="w-full border px-4 py-2 rounded-lg">
-      <textarea placeholder="Votre message" class="w-full border px-4 py-2 rounded-lg"></textarea>
-      <button class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700">Envoyer</button>
+    <form action="/Controllers/contact.php" class="max-w-xl mx-auto bg-white p-8 shadow-md rounded-lg space-y-4"  method="POST">
+      <input type="text" name="name" placeholder="Votre nom" class="w-full border px-4 py-2 rounded-lg">
+      <input type="email" name="email" placeholder="Votre email" class="w-full border px-4 py-2 rounded-lg">
+      <textarea placeholder="Votre message" name="message" class="w-full border px-4 py-2 rounded-lg"></textarea>
+      <button  type="submit" class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700">Envoyer</button>
     </form>
   </section>
 
